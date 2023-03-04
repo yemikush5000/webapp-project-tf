69ee629f2144c9ee7cb53f376fe65e70832c3a29
yemikush5000 <78047269+yemikush5000@users.noreply.github.com>
2023-03-04 15:59:58 +0000

    changes on main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "web01" {
-  ami  = "abcd000111xxxbb"
-  instance_type = "t2.micro"
+  ami               = "abcd000111xxxbb"
+  instance_type     = "t2.micro"
+  security_groups   = ["${aws_security_group.mygroup.id}"]
+  availability_zone = "eu-west-2" 
 }
