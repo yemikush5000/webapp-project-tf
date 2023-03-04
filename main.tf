94e25482ff768c78ffd0646132008688f3785406
yemikush5000 <78047269+yemikush5000@users.noreply.github.com>
2023-03-04 16:16:27 +0000

    changes to main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "web01" {
-  ami  = "abcd000111xxxbb"
-  instance_type = "t2.micro"
+  ami               = "abcd000111xxxbb"
+  instance_type     = "t2.micro"
+  tenancy           = "shared"
+  vpc_id            = "a012b345kj24"
+  availability_zone = "us-east-1a"
 }
